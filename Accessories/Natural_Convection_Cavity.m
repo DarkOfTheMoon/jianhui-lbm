164a4567b61f50e3ad2d14a3d3d712e935809a8d
yjhp1016 <yjhp1016@gmail.com>
2011-10-17 15:48:19 +0000

    update natural_convection.m

--- Accessories/Natural_Convection_Cavity.m
+++ Accessories/Natural_Convection_Cavity.m
@@ -3,13 +3,14 @@ Pr=0.71;                        %Prandtl number
 H=64;                          %Scale of lattice
 Ra=10000;                        %Rayleigh number
 dt=1;
-
+DeltaT=20;
 
 tau_f=0.5+(Ma*H*sqrt(3*Pr))/(dt*sqrt(Ra));
 niu_f=1/3*(tau_f-0.5)*dt;
 yix=niu_f/Pr;
-
+gbeta=Ra*niu_f*niu_f/(DeltaT*H*H*H*Pr);
 
 
 niu_f
 yix
+gbeta
