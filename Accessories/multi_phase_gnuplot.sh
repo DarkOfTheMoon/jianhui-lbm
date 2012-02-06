acf0402d7afde00ccf729590b2b65ce6b64eea64
yjhp1016 <yjhp1016@gmail.com>
2012-02-06 20:22:34 +0000

    add gnuplot script

---
+++ Accessories/multi_phase_gnuplot.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+gnuplot -persist<<EOF
+set multiplot
+set origin 0.0,0.0
+set size 0.333,0.5
+plot "$1Velocity_ave_max.txt" u 1 w l title "v_ave"
+
+
+
+set origin 0.333,0.0
+set size 0.333,0.5
+plot "$1Velocity_ave_max.txt" u 2 w l title "v_max"
+
+
+set origin 0.666,0.0
+set size 0.333,0.5
+plot "$1bodyforce.txt" u 1 w l title "saturation of 1"
+
+
+
+set origin 0.0,0.5
+set size 0.333,0.5
+plot "$1Relative_Permeability_Component1.txt" u 1 w l title "Rel_Perm_1"
+
+
+
+set origin 0.333,0.5
+set size 0.333,0.5
+plot "$1Relative_Permeability_Component2.txt" u 1 w l title "Rel_Perm_2"
+
+
+
+set origin 0.666,0.5
+set size 0.333,0.5
+plot "Relative_Permeability_Component1.txt" u 2:1 title "Rel_Perm_2"
+pause -1
