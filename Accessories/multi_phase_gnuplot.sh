c47f90c59ea9eb51a73de1ad98646455b5aa577a
yjhp1016 <yjhp1016@gmail.com>
2012-02-13 12:01:41 +0000

    add local_perm to multi_phase_gnuplot script

--- Accessories/multi_phase_gnuplot.sh
+++ Accessories/multi_phase_gnuplot.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 gnuplot -persist<<EOF
+
+
+
+
 set multiplot
+
 set origin 0.0,0.0
 set size 0.333,0.5
 plot "$1Velocity_ave_max.txt" u 1 w l title "v_ave"
@@ -26,11 +31,13 @@ plot "$1Relative_Permeability_Component1.txt" u 1 w l title "Rel_Perm_1"
 
 set origin 0.333,0.5
 set size 0.333,0.5
-plot "$1Relative_Permeability_Component2.txt" u 1 w l title "Rel_Perm_2"
+plot "$1Relative_Permeability_Component1.txt" u 3 w l,\
+"$1Relative_Permeability_Component2.txt" u 3 w l title "LOCAL_Rel_Perm_Both"
 
 
 
 set origin 0.666,0.5
 set size 0.333,0.5
-plot "$1Relative_Permeability_Component1.txt" u 2:1 title "Rel_Perm_2"
+plot "$1Relative_Permeability_Component1.txt" u 1 w l,\
+"$1Relative_Permeability_Component2.txt" u 1 w l title "Rel_Perm_Both"
 pause -1
