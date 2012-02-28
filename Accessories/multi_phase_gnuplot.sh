5ff89093d0aa9080050ee3516c33919718552fe3
yjhp1016 <yjhp1016@gmail.com>
2012-02-28 12:41:30 +0000

    add pressure both for MC plot sh

--- Accessories/multi_phase_gnuplot.sh
+++ Accessories/multi_phase_gnuplot.sh
@@ -25,7 +25,8 @@ plot "$1bodyforce.txt" u 1 w l title "saturation of 1"
 
 set origin 0.0,0.333
 set size 0.333,0.333
-plot "$1Relative_Permeability_Component1.txt" u 5 w l
+plot "$1Relative_Permeability_Component1.txt" u 5 w l title "pressure left",\
+"$1Relative_Permeability_Component1.txt" u 6 w l title "pressure right"
 #"$1Relative_Permeability_Component2.txt" u 3 title "LOCAL_Rel_Perm_Both"
 #plot "$1bodyforce.txt" u 2 w l title "saturation of 2"
 
