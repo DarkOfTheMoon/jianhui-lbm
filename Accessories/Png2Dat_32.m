90cfd6bcf9615eb80faa0a239b4e330c4978c370
yjhp1016 <yjhp1016@gmail.com>
2012-03-12 11:09:06 +0000

    modify PNG2DAT.m

--- Accessories/Png2Dat_32.m
+++ Accessories/Png2Dat_32.m
@@ -1,35 +1,56 @@
-input=imread('SD-Final.bmp','bmp');
+input=imread('circle_arms_bmp1.png','png');
+zlen=13;
 
 %===================================================
-%input=input/255;%====ONLY FOR PNG FILE READING======
+input=input/255;%====ONLY FOR PNG FILE READING======
 %===================================================
 
-%fid = fopen('Rocks.dat','wt');
-%if (fid==1)
-%   error('Cannot open image file...press CTRL-C to exit ');
-%end
-
-for i=1:5
-
-input1=[input,fliplr(input)];
-input2=flipud(input1);
-inputf=[input1;input2];
-[x,y]=size(inputf);
-
-input=inputf;
-
+fid = fopen('circle_arms_geometry.dat','wt');
+if (fid==1)
+  error('Cannot open image file...press CTRL-C to exit ');
 end
 
+inputf=input(:,:,1);
+inputf=inputf';
+[x,y]=size(inputf);
 
 
 
 
-%for i=1:x
-%    for j=1:y
- %       fprintf(fid,'%1d ',inputf(i,j));
- %   end
- %   fprintf(fid,'\n');
-%end
+% input=input./input;
+% a=input(:,:,1);
+
+% for i=1:5
+% 
+% input1=[input,fliplr(input)];
+% input2=flipud(input1);
+% inputf=[input1;input2];
+% [x,y]=size(inputf);
+% 
+% input=inputf;
+% 
+% end
 
 
-%fclose(fid);
+
+
+for k=1:zlen
+for j=1:y
+   for i=1:x
+       if (((k==1) || (k==zlen)) && (i>141))
+           fprintf(fid,'%1d\n',1);
+       else
+       fprintf(fid,'%1d\n',inputf(i,j));
+       end
+   end
+  
+end
+end
+
+x
+y
+zlen
+
+
+
+fclose(fid);
