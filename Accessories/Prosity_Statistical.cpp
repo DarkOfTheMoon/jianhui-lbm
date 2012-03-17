09cb04f41a0e2f65f38b92fa9000e77939bcb677
yjhp1016 <yjhp1016@gmail.com>
2012-03-17 00:25:46 +0000

    add porosity statistical tool in Accessories

---
+++ Accessories/Prosity_Statistical.cpp
@@ -0,0 +1,203 @@
+#include<iostream>
+#include<cmath>
+#include<cstdlib>
+#include<iomanip>
+#include<fstream>
+#include<sstream>
+#include<string>
+
+
+
+
+
+
+const int depth=6;
+using namespace std; 
+      
+int main (int argc , char * argv [])
+{
+
+int nx=300;
+int ny=300;
+int nz=300;
+
+int size[depth]={300,150,100,60,50,30};
+char poreFileName[128]="LV60.dat";
+char outputname[128]="prosity_output.dat";
+
+int num[depth];
+double ave[depth];
+double min[depth];
+double max[depth];
+
+int ls1,ls2,ls3;
+double** porosity;
+
+
+
+for (int i=0;i<depth;i++)
+        {
+                ls1=nx/size[i];
+                ls2=ny/size[i]; 
+                ls3=nz/size[i];
+                if (ls1<ls2)
+                        num[i]=ls1;
+                else    
+                        num[i]=ls2;
+                
+                if (ls3<num[i])
+                        num[i]=ls3;
+                //cout<<num[i]<<endl;
+        }
+
+int sum=0;
+
+
+bool*** Solid;
+
+
+	FILE *ftest;
+	ifstream fin;
+	
+	ftest = fopen(poreFileName, "r");
+
+	if(ftest == NULL)
+	{
+		cout << "\n The pore geometry file (" << poreFileName <<
+			") does not exist!!!!\n";
+		cout << " Please check the file\n\n";
+
+		exit(0);
+	}
+	fclose(ftest);
+
+	fin.open(poreFileName);
+
+
+double pore;
+	int i, j, k,ci,cj,ck;
+	
+	
+	Solid = new bool**[nx];
+	porosity = new double*[depth];
+		
+	for (i=0; i<nx;i++)
+	{
+	       Solid[i] = new bool*[ny];
+	       for (j=0;j<ny;j++)
+	       {
+	               Solid[i][j] = new bool[nz];
+	               for (k=0;k<nz;k++)
+	                       Solid[i][j][k] = 0;
+	       }
+	}
+		
+		for(int i=0;i<depth;i++)
+		        porosity[i]= new double[num[depth-1]*num[depth-1]*num[depth-1]];
+		
+		for (int i=0;i<depth;i++)
+		        for (int j=0;j<num[depth-1]*num[depth-1]*num[depth-1];j++)
+		                porosity[i][j]=0.0;
+		        
+		        
+		        
+		        //for (int i=0;i<depth;i++)
+		           //    cout<<num[i]<<endl;
+		        
+		
+		
+	for(k=0 ; k<nz ; k++)				///*********
+	for(j=0 ; j<ny ; j++)
+	for(i=0 ; i<nx ; i++)				///*********
+
+
+	
+		{	
+			
+			fin >> pore;
+			Solid[i][j][k]=pore;
+			if (pore==1.0)
+			{
+			        sum+=1;
+			        for (int ii=0;ii<depth;ii++)
+			                {
+			                        ls1=i/size[ii];
+			                        ls2=j/size[ii];
+			                        ls3=k/size[ii];
+			                        if ((ls1<num[ii]) and (ls2<num[ii]) and (ls3<num[ii]))
+			                                porosity[ii][ls1*num[ii]*num[ii]+ls2*num[ii]+ls3]+=1.0;
+			                        
+			                        
+			                }
+			}
+			
+			
+			
+		}
+		
+	fin.close();
+		
+	for (int i=0;i<depth;i++)
+	{        max[i]=0.0;
+	        ave[i]=0.0;
+	        min[i]=1.0;
+	}
+	
+	
+	for (int i=0;i<depth;i++)
+		        for (int j=0;j<num[depth-1]*num[depth-1]*num[depth-1];j++)
+		        porosity[i][j]=1-porosity[i][j]/(double)(size[i]*size[i]*size[i]);
+	
+	for (int i=0;i<depth;i++)
+	                {
+		        for (int j=0;j<num[i]*num[i]*num[i];j++)
+		        {
+		                if (porosity[i][j]<min[i])
+		                        min[i]=porosity[i][j];
+		                if (porosity[i][j]>max[i])
+		                        max[i]=porosity[i][j];
+		                
+		                ave[i]+=porosity[i][j];
+		                
+		
+		  
+		        } 
+		        
+		        ave[i]/=num[i]*num[i]*num[i];
+		        }
+		        
+	
+	cout<<"Porosity = "<<1-(double(sum)/(nx*ny*nz))<<endl;	
+	cout<<porosity[1][0]<<endl;
+	//cout<<sum<<endl;	
+	
+	ostringstream name;
+	name<<outputname;
+	ofstream out;
+	out.open(name.str().c_str());	
+	
+	for (int i=0;i<depth;i++)
+		        for (int j=0;j<num[i]*num[i]*num[i];j++)
+		        out<<size[i]<<" "<<porosity[i][j]<<endl;
+		
+		out.close();
+	
+	
+	ostringstream name2;
+	name2<<outputname<<"_sta";
+
+	out.open(name2.str().c_str());	
+	
+	for (int i=0;i<depth;i++)
+		        //for (int j=0;j<num[i]*num[i]*num[i];j++)
+		        out<<size[i]<<" "<<ave[i]<<"  "<<min[i]<<"  "<<max[i]<<endl;
+		
+		out.close();
+		
+		
+		
+		
+		
+}
+
+
