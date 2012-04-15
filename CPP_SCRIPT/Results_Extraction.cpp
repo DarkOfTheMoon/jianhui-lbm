bd5e615f7149970b3bd8f867fb0b6e6b54e56366
yjhp1016 <yjhp1016@gmail.com>
2012-04-15 09:59:37 +0000

    update read_extraction.cpp, add least square extraction

--- CPP_SCRIPT/Results_Extraction.cpp
+++ CPP_SCRIPT/Results_Extraction.cpp
@@ -16,10 +16,10 @@ int main (int argc , char * argv [])
 int total_number=5;
 char prefix[128]="rel_";    //prefix<<i<<"_Results.txt"
 char poreFileName[128]="Summary.outdat";
+char poreFileName2[128]="Summary_Least_Square.outdat";
 
 
-
-float var1,var2,var3;	
+float var1,var2,var3,var4,var5;	
 var1=0;var2=0;var3=0;
 char chd[128];
 int testd;
@@ -32,11 +32,13 @@ name2<<poreFileName;
 	ofstream out;
 int NCHAR=128;	
 char dummy[128+1];
+
 ofstream fins;	
 	fins.open(poreFileName,ios::trunc);
 	fins.close();
 	
-	
+	fins.open(poreFileName2,ios::trunc);
+	fins.close();
 	
 for (int i=1;i<=total_number;i++)
 {
@@ -57,6 +59,11 @@ for (int i=1;i<=total_number;i++)
 	else
 	        {
 	                
+	                
+	                
+
+	                
+	                
 	                fin.open(name.str().c_str());
 	                fin.getline(dummy, NCHAR);        //       The30800th computation result:     
 	                fin.getline(dummy, NCHAR);        //The Maximum velocity is: 0.0120153   Re_l=1.52508   Re_g=1.52508
@@ -64,8 +71,8 @@ for (int i=1;i<=total_number;i++)
 	                fin.getline(dummy, NCHAR);        //The max relative error of velocity is: 7.971677e-01
 	                fin>> chd >> chd>>chd>>chd >> chd>>chd>>chd>>var2;fin.getline(dummy, NCHAR);        //   The relative permeability of component 1 is -3.992098e+00, -3.079636e-03, 1.535446e-07
 	                fin >>chd >> chd>>chd>>chd >> chd>>chd>>chd>> var3;fin.getline(dummy, NCHAR);         //   The relative permeability of component 2 is -3.992098e+00, -3.079636e-03, 1.535446e-07
-	                fin.getline(dummy, NCHAR);            //The LOCAL relative permeability of component 1 is -1.108770e+01, -8.600400e-03, -3.606825e-08
-	                fin.getline(dummy, NCHAR);        //The LOCAL relative permeability of component 2 is 2.432098e+02, 1.871785e-02, -4.650501e-06
+	                fin>> chd >> chd>>chd>>chd >> chd>>chd>>chd>>chd>>chd>>var4;fin.getline(dummy, NCHAR);            //The LEAST SQUARED relative permeability of component 1 is 1.594603e-01
+	                fin >>chd >> chd>>chd>>chd >> chd>>chd>>chd>> chd>>chd>>var5;fin.getline(dummy, NCHAR);        //The LEAST SQUARED relative permeability of component 2 is 4.766752e-01
 	                fin>> chd>>chd>>chd >> chd>>var1;fin.getline(dummy, NCHAR);    //  Satuation of Component 1: 8.510638e-02, The satuation of Component 2: 9.148936e-0
 	                fin.getline(dummy, NCHAR);        //The relative error of permiability computing is: 3.423860e-03
 	                fin.getline(dummy, NCHAR);            //Elapsed time is 0h3m22s
@@ -78,8 +85,8 @@ for (int i=1;i<=total_number;i++)
 	                fin.getline(dummy, NCHAR);        //The max relative error of velocity is: 7.971677e-01
 	                fin.getline(dummy, NCHAR);         //   The relative permeability of component 1 is -3.992098e+00, -3.079636e-03, 1.535446e-07
 	                fin.getline(dummy, NCHAR);        //The relative permeability of component 2 is 2.143594e+02, 1.349168e-02, -5.743910e-05
-	                fin.getline(dummy, NCHAR);            //The LOCAL relative permeability of component 1 is -1.108770e+01, -8.600400e-03, -3.606825e-08
-	                fin.getline(dummy, NCHAR);        //The LOCAL relative permeability of component 2 is 2.432098e+02, 1.871785e-02, -4.650501e-06
+	                fin.getline(dummy, NCHAR);            //The LEAST SQUARED relative permeability of component 1 is 1.594603e-01
+	                fin.getline(dummy, NCHAR);        //The LEAST SQUARED relative permeability of component 2 is 4.766752e-01
 	                fin.getline(dummy, NCHAR);    //  Satuation of Component 1: 8.510638e-02, The satuation of Component 2: 9.148936e-0
 	                fin.getline(dummy, NCHAR);        //The relative error of permiability computing is: 3.423860e-03
 	                fin.getline(dummy, NCHAR);            //Elapsed time is 0h3m22s
@@ -90,9 +97,13 @@ for (int i=1;i<=total_number;i++)
 	                out<<var1<<" "<<var2<<" "<<var3<<endl;
 	                out.close();
 	                
+	            
+	                
+	                ofstream out2(poreFileName2,ios::app);
+	                out2<<var1<<" "<<var4<<" "<<var5<<endl;
+	                out2.close();
+	                
 	                fin.close();
-	                
-	                
 	            
 	                
 	        }
