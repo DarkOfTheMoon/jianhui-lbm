9c06018fdb5a89455acf92f4392bebc9c4acae4e
yjhp1016 <yjhp1016@gmail.com>
2012-05-13 11:15:32 +0000

    add saturation _check

---
+++ Accessories/saturation_check.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+gnuplot -persist<<EOF
+
+
+
+
+set multiplot
+
+set origin 0.0,0.0
+set size 0.5,1.0
+plot "$1bodyforce.txt" u 1 w l title "saturation of 1"
+
+
+set origin 0.5,0.0
+set size 0.5,1.0
+plot "$1bodyforce.txt" u 3 w l title "saturation error"
+
+
+pause -1
